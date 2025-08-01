5d7a76a96db5358439beb62c798483bc6d3f6312
Joseph Baptista <josephedbaptistaca@gmail.com>
2025-08-01 00:46:16 -0400

    changes to middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,33 +1,26 @@
 import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware((context, next) => {
-    console.log('\n\n middleware');
-    const sessionToken = context.cookies.get('session_token')?.value;
-    const UNPROTECTED_ROUTES = [
-        /^\/login($|\/.*)/, // matches /login and any query params that may be included
-        /^\/test($|\/.*)/, // matches /login and any query params that may be included
-        /^\/signup($|\/.*)/,
-        /^\/invite($|\/.*)/,
-        /^\/hello/,
-        /^\/500($|\/.*)/,
-        /^\/400($|\/.*)/,
-    ];    
-    const isSafeRoute = (path: string): boolean => {
-        return UNPROTECTED_ROUTES.some((pattern) => pattern.test(path));
-    };
+const UNPROTECTED_ROUTES = [
+    /\/login($|\/.*)/,
+    /\/signup($|\/.*)/,
+    /\/test($|\/.*)/,
+    /\/500($|\/.*)/,
+    /\/400($|\/.*)/,
+];
 
+const isSafeRoute = (path: string): boolean => {
+    return UNPROTECTED_ROUTES.some((pattern) => pattern.test(path));
+};
+
+export const onRequest = defineMiddleware((context, next) => {
     if (isSafeRoute(context.url.toString())) {
         return next();
     }
 
+    const sessionToken = context.cookies.get('session_token')?.value;
     if (!sessionToken) {
-        return context.redirect(context.url.origin +  "/test");
-        // context.url = new URL(context.url.origin +  "/test");
-        // return next(new Request(new URL(context.url.origin +  "/test"), {
-        //     headers: {
-        //         "x-redirect-to": context.url.pathname
-        //     }
-        // }));
+        context.request.headers.set('x-redirect-to', context.url.pathname);
+        return context.redirect(context.url.origin +  '/test');
     }
 
     return next();
