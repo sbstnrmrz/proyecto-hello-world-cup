a6a45c0b7d52ed64deeb242c8d5685d57f96d485
sbstnrmrz <ramicastasebas@gmail.com>
2025-08-01 03:55:38 -0400

    added cookie in the backend

--- server/main.go
+++ server/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"crypto/rand"
 	"database/sql"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
 )
+
+func GenerateSessionToken() string {
+    b := make([]byte, 32)
+    rand.Read(b)
+    return base64.URLEncoding.EncodeToString(b)
+}
+
+
 
 func main()  {
 	db, err := sql.Open("sqlite3", "database.db");
@@ -18,6 +29,8 @@ func main()  {
 		log.Fatalln("error opening db:", err);
 	}
 	defer db.Close()
+
+	var sessions = make(map[string]string)
 
 	router := mux.NewRouter()
 
@@ -73,6 +86,16 @@ func main()  {
 			http.Error(w, fmt.Sprintf("Username or password incorrect"), http.StatusBadRequest)
 			return
 		}
+
+		sessionToken := GenerateSessionToken()
+		sessions[sessionToken] = username
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     "session_token",
+			Value:    sessionToken,
+			Expires:  time.Now().Add(24 * time.Hour),
+			HttpOnly: true,
+		})
 
 		http.Error(w, fmt.Sprintf("Login successfull"), http.StatusOK)
 	}).Methods("POST")
